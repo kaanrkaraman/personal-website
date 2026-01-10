36cfe4c0c0c34decd6ee92488cac678323989647
Kaan <kaanrkaraman@proton.me>
2026-01-10 21:42:19 +0300

    Add security headers

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Buffer } from "buffer";
+import App from "./App";
+import "./index.css";
+
+window.Buffer = Buffer;
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
